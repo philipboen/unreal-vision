8bd723443a6466944c0b1124833d61ba6d901771
Philip Boen <philipboen001@gmail.com>
2024-07-21 23:35:53 +0300

    chore: update landing page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ import { Figtree } from "next/font/google";
 const figtree = Figtree({ subsets: ["latin"], variable: "--font-figtree" });
 
 export const metadata: Metadata = {
-  title: "Dall-E Image Generation",
-  description: "Generate images using OpenAI's Dall-E model.",
+  title: "Unreal Vision",
+  description:
+    "Unreal Vision - Art beyond Reality is an innovative web application that leverages advanced AI technology to transform user prompts into unique, captivating images. This platform empowers users to explore the boundaries of creativity and visual art, making the process of art creation accessible and engaging for everyone.",
 };
 
 export default function RootLayout({
