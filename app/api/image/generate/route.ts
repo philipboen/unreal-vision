e7b2fca5771ab50931d8dd82552cdeca3004f3a4
Philip Boen <philipboen001@gmail.com>
2024-07-22 23:02:48 +0300

    refactor: use prettier to format route.ts

--- app/api/image/generate/route.ts
+++ app/api/image/generate/route.ts
@@ -1,30 +1,29 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (request: Request) => {
-    const { prompt } = await request.json();
+  const { prompt } = await request.json();
 
-    try {
-        const response = await fetch(
-            "https://api.openai.com/v1/images/generations",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-                },
-                body: JSON.stringify({
-                    model: "dall-e-3",
-                    prompt,
-                    
-                }),
-            }
-        );
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "dall-e-3",
+          prompt,
+        }),
+      }
+    );
 
-        const image = await response.json();
-        const imageUrl = image.data[0].url;
+    const image = await response.json();
+    const imageUrl = image.data[0].url;
 
-        return NextResponse.json({ imageUrl });
-    } catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-}
+    return NextResponse.json({ imageUrl });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
